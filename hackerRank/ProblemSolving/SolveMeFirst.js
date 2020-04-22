4f049d9e6bf8394bb71287546afa4e0851f733ac
bomeekim <thespringg@naver.com>
2020-04-22 23:08:55 +0900

    [Hackerrank] Solve Me First

---
+++ hackerRank/ProblemSolving/SolveMeFirst.js
@@ -0,0 +1,12 @@
+/**
+ * Solve Me First
+ * https://www.hackerrank.com/challenges/solve-me-first/problem
+ */
+
+const testCase0 = solveMeFirst(2, 3);
+console.log(testCase0);
+
+function solveMeFirst(a, b) {
+  // Hint: Type return a+b below
+  return a+b;
+}
