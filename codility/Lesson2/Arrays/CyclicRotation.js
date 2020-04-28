0ea06917b45e49cb9b8ac02d9e64c50f9e6edc44
bomeekim <thespringg@naver.com>
2020-04-29 00:06:52 +0900

    [Codility][Lesson2] CyclicRotation Score 12%

---
+++ codility/Lesson2/Arrays/CyclicRotation.js
@@ -0,0 +1,42 @@
+/**
+ * Lesson2 Arrays: CyclicRotation
+ * Rotate an array to the right by a given number of steps.
+ * 
+ * https://app.codility.com/programmers/lessons/2-arrays/https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+ */
+
+const testCase0 = solution([3, 8, 9, 7, 6], 3);
+// const testCase1 = solution([0, 0, 0], 1);
+// const testCase2 = solution([1, 2, 3, 4], 4);
+
+console.log(testCase0);
+// console.log(testCase1);
+// console.log(testCase2);
+
+function solution(A, K) {
+  // 배열과 K의 값이 같거나, 배열에 같은 요소가 있는 경우 A 그대로 리턴한다.
+  if ((A.length === K)
+    || (new Set(A).size !== A.length)) {
+    return A;
+  }
+
+  let array = [];
+  let keepArray = [];
+
+  for (let i=A.length - 1; i>=0; i--) {
+    array.push(A[i]);
+
+    if (i >= K-1) {
+      keepArray = [].concat(...array);
+    }
+  }
+
+  keepArray.reverse();
+
+  array.splice(0, K);
+  array.reverse();
+
+  keepArray.push(...array);
+
+  return keepArray;
+}
